883d24d1a86faf92b23978d003e28b7a3ef14cbe
Craig-San <62339060+Craig-San@users.noreply.github.com>
2024-05-30 21:32:48 +0200

    Update streamlit_crop_app.py

--- streamlit_crop_app.py
+++ streamlit_crop_app.py
@@ -15,6 +15,7 @@ def check_password():
     def login_form():
         """Form with widgets to collect user information"""
         with st.form("Credentials"):
+            st.text("Please Enter Your Credentials")
             st.text_input("Username", key="username")
             st.text_input("Password", type="password", key="password")
             st.form_submit_button("Log in", on_click=password_entered)
